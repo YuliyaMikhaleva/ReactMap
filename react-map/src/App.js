9d2899fcf55cb589ebc0184994221f195379022c
YuliyaMikhaleva <umihaleva93@mail.ru>
2023-07-03 12:01:01 +0500

    added markers with click

--- react-map/src/App.js
+++ react-map/src/App.js
@@ -1,58 +1,79 @@
 import './App.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import {useState} from "react";
+import { MapContainer, TileLayer, Marker,   useMapEvent, } from 'react-leaflet';
+import {useState, useCallback, useEffect} from "react";
 
-function App() {
 
-    const [activePark, setActivePark] = useState(null);
+
+function App(type, handler) {
+
+
+
+    // const [activePark, setActivePark] = useState(null);
     const handlePopupClose = (e) => {
         console.log(e.popup)
     }
+    // const addMarker = () => {
+    //     console.log('e')
+    // }
+
+    const [map, setMap] = useState(null)
+    // const onClick = useCallback(() => {
+    //     // map.setView(center, zoom)
+    //     console.log('click')
+    // }, [map])
+    const [coords, setCoords] = useState([58.020434,56.253091]);
+    const [markers, setMarkers] = useState([]);
+    const handleLocationFound = (e) => {
+        setCoords([e.latitude, e.longitude]);
+    };
+
+    function Click({onLocationFound}) {
+        const handleLocationFound = (e) => {
+            setCoords([e.latitude, e.longitude]);
+        };
+        const [position, setPosition] = useState(null)
+        const map = useMapEvent({
+            click: (e) => {
+                map.locate()
+                // console.log('e', `[${e.latlng.lat},${e.latlng.lng}]`)
+                if (e.latlng && e.latlng.lat && e.latlng.lng){
+                    setMarkers([...markers,[e.latlng.lat, e.latlng.lng]])
+
+                }
+            },
+        })
+        // return null;
+    }
+
+    useEffect(() => {
+        console.log(markers)
+    },[markers])
+
   return (
     <div className="App">
         {/*долгота и широта*/}
-        <MapContainer center={[58.020434,56.253091]} zoom={12} scrollWheelZoom={false} onPopupClose={handlePopupClose}>
+        <MapContainer
+            center={[58.020434,56.253091]}
+            zoom={12}
+            scrollWheelZoom={false}
+            onPopupClose={handlePopupClose}
+            tap={false}
+            style={{position:'relative', zIndex:1}}
+            onMouseDown={(e) => e.preventDefault()}
+            ref={setMap}
+        >
+            <Click onLocationFound={handleLocationFound} />
             {/*авторство*/}
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
             {/*маркер с координатами*/}
-            <Marker
-                position={[58.020434, 56.253091]}
-                eventHandlers={{
-                    click: (e) => {
-                        if (activePark){
-                            setActivePark(true)
-                        } else if (activePark === null) {
-                            setActivePark(true)
-                        } else {
-                            setActivePark(true)
-                        }
-                    },
-                }}
-            />
-
-            {activePark && (
-                <>
-                <div>1111</div>
-                    <Popup
-                        interactive
-                        position={
-                            [ 58.020434,56.253091]
-                        }
-                        onClose={(e) => {
-                            console.log('close')
-                            setActivePark(!activePark);
-                        }}
-                    >
-                        <div>
-                            <h2>Попап</h2>
-                        </div>
-                    </Popup>
-                </>
-
-            )}
+            {markers.map((marker, index) => (
+                <Marker key={index} position={marker}>
+                    {/*<Popup>Маркер {marker.id}</Popup>*/}
+                </Marker>
+            ))}
         </MapContainer>
     </div>
   );
