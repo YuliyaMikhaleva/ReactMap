d83ead3ee1b3a74ab4e9ebe82f0bb6046337a0c6
YuliyaMikhaleva <umihaleva93@mail.ru>
2023-07-05 13:30:28 +0500

    added 2 points to map

--- react-map/src/App.js
+++ react-map/src/App.js
@@ -1,85 +1,86 @@
 import './App.css';
-import { MapContainer, TileLayer, Marker,   useMapEvent, Polyline } from 'react-leaflet';
-import {useState, useCallback, useEffect} from "react";
+// import Map, {Marker} from 'react-map-gl';
+import Map, {
+    Marker,
+    Popup,
+    NavigationControl,
+    FullscreenControl,
+    ScaleControl,
+    GeolocateControl
+} from 'react-map-gl';
+import MapGl from 'react-map-gl';
+// import ReactMapGL, { Marker, NavigationControl, GeolocateControl, Popup } from 'react-map-gl';
+import {useState, useEffect, useMemo} from "react";
+import React from "react";
+import THEATRES from './theatres.json';
+import Pin from "./Pin";
 
+function App(){
+    const [popupInfo, setPopupInfo] = useState(null);
+    const TOKEN = 'pk.eyJ1IjoiY2hlY2h1cm92IiwiYSI6ImNsYmxiMXp1ZDA1a28zdnFxNGJnbHR3Z2QifQ.7tfOUn-31oHBDA7_GPN2ow'; // Set your mapbox token here
 
-
-function App(type, handler) {
-
-
-
-    // const [activePark, setActivePark] = useState(null);
-    const handlePopupClose = (e) => {
-        console.log(e.popup)
-    }
-    // const addMarker = () => {
-    //     console.log('e')
-    // }
-
-    const [map, setMap] = useState(null)
-    // const onClick = useCallback(() => {
-    //     // map.setView(center, zoom)
-    //     console.log('click')
-    // }, [map])
-    const [coords, setCoords] = useState([58.020434,56.253091]);
-    const [markers, setMarkers] = useState([]);
-    const handleLocationFound = (e) => {
-        setCoords([e.latitude, e.longitude]);
-    };
-
-    function Click({onLocationFound}) {
-        const handleLocationFound = (e) => {
-            setCoords([e.latitude, e.longitude]);
-        };
-        const [position, setPosition] = useState(null)
-        const map = useMapEvent({
-            click: (e) => {
-                map.locate()
-                // console.log('e', `[${e.latlng.lat},${e.latlng.lng}]`)
-                if (e.latlng && e.latlng.lat && e.latlng.lng){
-                    setMarkers([...markers,[e.latlng.lat, e.latlng.lng]])
-
-                }
-            },
-        })
-        // return null;
-    }
-
-    useEffect(() => {
-        console.log(markers)
-    },[markers])
-
-    const coordinates = markers.map((marker) => marker);
+    const pins = useMemo(
+        () =>
+            THEATRES.map((city, index) => (
+                <Marker
+                    key={`marker-${index}`}
+                    longitude={city.longitude}
+                    latitude={city.latitude}
+                    anchor="bottom"
+                    onClick={e => {
+                        // If we let the click event propagates to the map, it will immediately close the popup
+                        // with `closeOnClick: true`
+                        e.originalEvent.stopPropagation();
+                        setPopupInfo(city);
+                    }}
+                >
+                    <Pin />
+                </Marker>
+            )),
+        []
+    );
 
   return (
     <div className="App">
-        {/*долгота и широта*/}
-        <MapContainer
-            center={[58.020434,56.253091]}
-            zoom={12}
-            scrollWheelZoom={false}
-            onPopupClose={handlePopupClose}
-            tap={false}
-            style={{position:'relative', zIndex:1}}
-            onMouseDown={(e) => e.preventDefault()}
-            ref={setMap}
-        >
-            <Click onLocationFound={handleLocationFound} />
-            {/*авторство*/}
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
-            {/*маркер с координатами*/}
-            {markers.map((marker, index) => (
-                <Marker key={index} position={marker}>
-                    {/*<Popup>Маркер {marker.id}</Popup>*/}
-                </Marker>
-            ))}
+        <div id="map">
+            <Map
+                mapboxAccessToken={TOKEN}
+                initialViewState={{
+                    latitude: 58.009209,
+                    longitude: 56.221997,
+                    zoom: 13,
+                    bearing: 0,
+                    pitch: 0
+                }}
+                mapStyle="mapbox://styles/chechurov/cldj0qx1b002h01o65uimgmsm"
+            >
+                <GeolocateControl position="top-left" />
+                <FullscreenControl position="top-left" />
+                <NavigationControl position="top-left" />
+                <ScaleControl />
+                {pins}
+                {popupInfo && (
+                    <Popup
+                        anchor="top"
+                        longitude={Number(popupInfo.longitude)}
+                        latitude={Number(popupInfo.latitude)}
+                        onClose={() => setPopupInfo(null)}
+                    >
+                        <div>
+                            {popupInfo.city}, {popupInfo.state} |{' '}
+                            <a
+                                target="_new"
+                                href={`http://en.wikipedia.org/w/index.php?title=Special:Search&search=${popupInfo.city}, ${popupInfo.state}`}
+                            >
+                                Wikipedia
+                            </a>
+                        </div>
+                        <img width="100%" src={popupInfo.image} />
+                    </Popup>
+                )}
+            </Map>
+        </div>
 
-            {coordinates.length > 1 && <Polyline positions={coordinates} color="blue" smoothFactor={1.5} />}
-
-        </MapContainer>
     </div>
   );
 }
